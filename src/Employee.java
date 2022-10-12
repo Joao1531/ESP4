fe113478641d03a82c6cb01330e08fb907bd224b
Joao <jpdu.lopes@fct.unl.pt>
2022-10-12 22:59:26 +0100

    CGI Trabalho Pratico 4

---
+++ src/Employee.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Employee extends UniversityMember {
+
+    /**
+     * Default constructor
+     */
+    public Employee() {
+    }
+
+    /**
+     * 
+     */
+    private int acctNo;
+
+    /**
+     * @return
+     */
+    public int getAcctNo() {
+        return acctNo;
+    }
+
+}
